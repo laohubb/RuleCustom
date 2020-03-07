7bd49cef77f9915bda632a2033fb7477caea0e3f
laohu <laohubbb@gmail.com>
2020-03-07 08:47:16 +0800

    Update checkin.js

--- Surge/checkin.js
+++ Surge/checkin.js
@@ -10,8 +10,7 @@ $tool.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=m
     let isAM = date.getHours() < 12 ? true : false;
     let title = 'Clock' + (isAM ? ' in' : ' out') + (isAM ? ' ' : ' ');
     let subtitle = '';
-    let content = 'shortcuts://run-shortcut?name=Check%20in';
-    let content = 'dingtalk://dingtalkclient/page/link?url=http://122.226.181.104:12208/H5/index/Index.htm?OP=phone_html5&r=20200306031619356';
+    let content = 'shortcuts://run-shortcut?name=Check%20in''dingtalk://dingtalkclient/page/link?url=http://122.226.181.104:12208/H5/index/Index.htm?OP=phone_html5&r=20200306031619356';
     if (!error) {
         if (obj && obj.length > 1) {
             let yi = obj[1];
